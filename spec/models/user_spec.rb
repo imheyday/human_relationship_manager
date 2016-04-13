850ce0d470fe5946fc13954a8194d5da20e27d01
Blanchard François <francois.blanchard1@gmail.com>
2016-04-13 23:06:20 +0200

    add clearance gem

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe User do
   describe '#create' do
     it 'be valid' do
-      user = build(:user)
-      expect(user.valid?).to eq true
+      basic_user = build(:user)
+      expect(basic_user.valid?).to eq true
     end
   end
 end
