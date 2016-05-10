c5991143b9621934dcaf751cab4e9a6ff62dbe32
Blanchard François <francois.blanchard1@gmail.com>
2016-05-10 22:28:41 +0200

    add job applications actions

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -5,9 +5,27 @@ class JobApplicationsController < ApplicationController
     @job_applications = JobApplication.all
   end
 
+  def create
+    @job_application = JobApplication.new(job_application_params)
+    if @job_application.save
+      redirect_to job_application_path(@job_application)
+    else
+      render :new
+    end
+  end
+
+  def update
+    find_candidate
+    if @job_application.update_attributes(job_application_params)
+      redirect_to job_application_path(@job_application)
+    else
+      render :edit
+    end
+  end
+
   private
 
-  def candidate_params
+  def job_application_params
     params.require(:job_application).permit(:received_at, :cv_file, :candidate_id)
   end
 
---
+++ spec/controllers/job_applications_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe JobApplicationsController do
+  describe '#create' do
+    it 'render show page' do
+      allow(controller).to receive(:signed_in?).and_return(true)
+      post :create, job_application: FactoryGirl.attributes_for(:job_application)
+      expect(response).to redirect_to job_application_path(1)
+    end
+  end
+  describe '#update' do
+    it 'render edit page' do
+      allow(controller).to receive(:signed_in?).and_return(true)
+      job_application = FactoryGirl.create(:job_application)
+      job_application.received_at = 1.day.ago
+      post :update, id: job_application.id, job_application: job_application.attributes
+      expect(response).to redirect_to job_application_path(1)
+    end
+  end
+end
