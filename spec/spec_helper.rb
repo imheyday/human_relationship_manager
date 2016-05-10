89c7631cbc2ec74263b2135e8add281c3353ff66
Blanchard François <francois.blanchard1@gmail.com>
2016-05-10 22:37:35 +0200

    remove all domains mock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
 
 require 'webmock/rspec'
-WebMock.disable_net_connect!(allow_localhost: true)
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
