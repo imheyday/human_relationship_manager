71039dc7c5dff97016ec0f8917457820e4eabc51
Blanchard François <francois.blanchard1@gmail.com>
2016-05-10 22:29:19 +0200

    mock google userinfo

--- spec/services/user_authentication_spec.rb
+++ spec/services/user_authentication_spec.rb
@@ -11,6 +11,16 @@ RSpec.describe UserAuthentication do
   end
   describe '#auth!' do
     it 'authenticate user with token' do
+      userinfo = File.read 'spec/support/responses/google_userinfo.json'
+      stub_request(:get, 'https://www.googleapis.com/oauth2/v1/userinfo?access_token=1234AZERTY&alt=json').with(
+        headers: {
+          'Accept' => '*/*',
+          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent' => 'Ruby'
+        }
+      ).to_return(
+        status: 200, body: userinfo, headers: {}
+      )
       oauth_object = double(:oauth, access_token: '1234AZERTY', refresh_token: 'REFRESH1234')
       user_authentication = UserAuthentication.new(oauth_object)
       user_authentication.auth!
